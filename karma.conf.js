17693d49040c6ebd5b4f706b625fb99cc8c01f34
digijin <james@digijin.com>
2017-10-14 12:16:13 +0100

    moving to xvfb

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,26 @@
-const isDocker = require('is-docker')();
+const isDocker = require("is-docker")();
 
-module.exports = config => config.set({
-  customLaunchers: {
-    ChromeCustom: {
-      base: 'ChromeHeadless',
-      // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
-      // more permissions than Docker allows by default)
-      flags: isDocker ? ['--no-sandbox'] : []
-    }
-  },
-  files: [
-    'test/*.js'
-  ],
-  // This example uses mocha and chai, but we could use something else, jasmine for example
-  frameworks: ['mocha', 'chai'],
-  plugins: [
-    require('karma-mocha'),
-    require('karma-chai'),
-    require('karma-chrome-launcher'),
-    require('karma-mocha-reporter'),
-    require('karma-junit-reporter')
-  ],
-  junitReporter: {
-    outputDir: 'build'
-  }
-});
+module.exports = config =>
+	config.set({
+		customLaunchers: {
+			ChromeCustom: {
+				base: "Chrome",
+				// We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+				// more permissions than Docker allows by default)
+				flags: isDocker ? ["--no-sandbox"] : []
+			}
+		},
+		files: ["test/*.js"],
+		// This example uses mocha and chai, but we could use something else, jasmine for example
+		frameworks: ["mocha", "chai"],
+		plugins: [
+			require("karma-mocha"),
+			require("karma-chai"),
+			require("karma-chrome-launcher"),
+			require("karma-mocha-reporter"),
+			require("karma-junit-reporter")
+		],
+		junitReporter: {
+			outputDir: "build"
+		}
+	});
